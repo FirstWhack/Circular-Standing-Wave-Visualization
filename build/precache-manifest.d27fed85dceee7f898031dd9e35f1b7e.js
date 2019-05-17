44cbd4a0abd55d61e810387ac68c84ad7e6a1370
Josiah South <josiahdsouth@gmail.com>
2019-05-17 17:32:26 -0400

    Fix GH pages URLs

---
+++ build/precache-manifest.d27fed85dceee7f898031dd9e35f1b7e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7abdc3b8611c22355e83ec0cc0423b4",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "1f8c9a554a4732c07320",
+    "url": "/build/static/js/2.7c1f1192.chunk.js"
+  },
+  {
+    "revision": "20be4771d68c5e8ceeff",
+    "url": "/build/static/js/main.55b87195.chunk.js"
+  },
+  {
+    "revision": "b4d8ef2cb7791815737b",
+    "url": "/build/static/js/runtime~main.9df7376b.js"
+  }
+]);
